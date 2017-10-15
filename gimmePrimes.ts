d295005be20e2f880d9bcd446f660fb0fcb168a8
Achim Gruen <achim.gruen@gmail.com>
2017-10-15 21:16:08 +0200

    simple soluton for memory problem

--- gimmePrimes.ts
+++ gimmePrimes.ts
@@ -2,26 +2,60 @@
 
 console.log('starting...');
 
-const MAX = 100;
-
+let start = 2;
+const BY = 10000000;
+const MAX = 100000000;
+let cnt = BY;
 let pool = new Map<number, null>();
-for (let i = 2; i < MAX; i++) {
-    pool.set(i, null);
+
+let optimized = true;
+
+if (optimized) {
+
+    while (cnt <= MAX) {
+
+        for (let i = start; i < cnt; i++) {
+            pool.set(i, null);
+        }
+    
+        pool.forEach((value: null, key: number) => {    
+            let multiplier = key;
+            let product = 0;
+            while (product < cnt) {
+                if (pool.has(product)) {
+                    pool.delete(product);
+                }
+                product = key * multiplier;
+                multiplier++;        
+            }
+        });
+
+        console.log(pool.size + ' prime numbers found between 0 and ' + cnt);
+        start = cnt;
+        cnt += BY;
+    }
+
+} else {
+
+    for (let i = 2; i < MAX; i++) {
+        pool.set(i, null);
+    }
+
+    pool.forEach((value: null, key: number) => {    
+        let multiplier = key;
+        let product = 0;
+        while (product < MAX) {
+            if (pool.has(product)) {
+                pool.delete(product);
+            }
+            product = key * multiplier;
+            multiplier++;        
+        }
+    });
+
+    console.log(pool.size + ' prime numbers found between 0 and ' + MAX);
 }
 
-pool.forEach((value: null, key: number) => {    
-    let multiplier = key;
-    let product = 0;
-    while (product < MAX) {
-        if (pool.has(product)) {
-            pool.delete(product);
-        }
-        product = key * multiplier;
-        multiplier++;        
-    }
-});
-
-console.log(pool.keys());
-console.log(pool.size + ' prime numbers found between 0 and ' + MAX);
+// console.log(pool.keys());
 
 console.log('...finished');
